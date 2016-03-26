366f8ae733050e93208a5e02a5447e8550de04b3
zhouzaomiao <zzmiao@gmail.com>
2016-03-26 11:30:30 +0800

    add

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,7 +25,7 @@ set :rvm_type, :user
 set :keep_releases, 5
 
 # files we want symlinking to specific entries in shared
-set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/database.yml config/application.yml}
 
 # dirs we want symlinking to shared
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
